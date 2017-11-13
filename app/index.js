2d376dd668b5a4e666c0dbde2f14d2bf616c44f5
shivprogrammer <shiv.unadkat@gmail.com>
2017-11-13 14:16:53 -0800

    after 3 days of failing wI found our winner. [IOS IS WORKING]

---
+++ app/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { AppContainer } from '~/containers'
+
+export default function ShivvyTime (props) {
+  return (
+    <AppContainer />
+  )
+}
---
+++ index.android.js
@@ -0,0 +1,4 @@
+import ShivvyTime from './app'
+import { AppRegistry } from 'react-native'
+
+AppRegistry.registerComponent('ShivvyTime', () => ShivvyTime)
