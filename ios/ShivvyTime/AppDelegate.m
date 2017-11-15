77cd3a2623c1dfa253da9b9e8b7a55cf1a16fab2
shivprogrammer <shiv.unadkat@gmail.com>
2017-11-14 18:19:02 -0800

    back to when both simulators working

--- ios/ShivvyTime/AppDelegate.m
+++ ios/ShivvyTime/AppDelegate.m
@@ -12,8 +12,6 @@
 #import "RCTBundleURLProvider.h"
 #import "RCTRootView.h"
 
-#import <FBSDKCoreKit/FBSDKCoreKit.h>
-
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -34,19 +32,6 @@
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
   return YES;
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-  [FBSDKAppEvents activateApp];
-}
-- (BOOL)application:(UIApplication *)application
-            openURL:(NSURL *)url
-  sourceApplication:(NSString *)sourceApplication
-         annotation:(id)annotation {
-  return [[FBSDKApplicationDelegate sharedInstance] application:application
-                                                         openURL:url
-                                               sourceApplication:sourceApplication
-                                                      annotation:annotation];
 }
 
 @end
