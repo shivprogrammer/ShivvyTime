78bd6f6758d23539a643f09571d01a56f4637357
shivprogrammer <shiv.unadkat@gmail.com>
2017-11-16 00:16:18 -0800

    added authentication function in authentication.js

---
+++ app/redux/modules/authentication.js
@@ -0,0 +1,12 @@
+const initialState = {
+  isAuthed: false,
+  isAuthenticating: false,
+  autheId: ''
+}
+
+export default function authentication (state = intitialState, action) {
+  switch (action.type) {
+    default :
+    return state
+  }
+}
