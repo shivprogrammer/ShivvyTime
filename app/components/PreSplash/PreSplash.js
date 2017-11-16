15cb7285ddb0a3e9f720934c0f8d788146acd76a
shivprogrammer <shiv.unadkat@gmail.com>
2017-11-15 20:13:39 -0800

    added PreSplash component

---
+++ app/components/PreSplash/PreSplash.js
@@ -0,0 +1,16 @@
+import React, { PropTypes, Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class PreSplash extends Component {
+  static propTypes = {}
+  state = {}
+  render () {
+    return (
+      <View>
+        <Text>
+          PreSplash
+        </Text>
+      </View>
+    )
+  }
+}
