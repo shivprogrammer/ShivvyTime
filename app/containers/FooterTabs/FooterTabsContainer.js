5d331e7c3531fc9a82ddcf31da7b070349aca2f7
shivprogrammer <shiv.unadkat@gmail.com>
2017-11-18 01:20:34 -0800

    added FooterTabsContainer

---
+++ app/containers/FooterTabs/FooterTabsContainer.js
@@ -0,0 +1,16 @@
+import React, { PropTypes, Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class FooterTabsContainer extends Component {
+  static PropTypes = {}
+  state = {}
+  render() {
+    return (
+      <View>
+        <Text>
+          FooterTabsContainer
+        </Text>
+      </View>
+    )
+  }
+}
