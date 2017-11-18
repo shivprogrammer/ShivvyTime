a8ea0c420177262e2763fa16af5809b554caf825
shivprogrammer <shiv.unadkat@gmail.com>
2017-11-18 01:14:44 -0800

    started FooterTabs.ios.js file

---
+++ app/components/FooterTabs/FooterTabs.ios.js
@@ -0,0 +1,16 @@
+import React, { PropTypes } from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+FooterTabs.propTypes = {
+
+}
+
+export default function FooterTabs (props) {
+  return (
+    <View>
+      <Text>
+        FooterTabs
+      </Text>
+    </View>
+  )
+}
