752f8c86afd752e2173464201cad3204a7081199
shivprogrammer <shiv.unadkat@gmail.com>
2017-11-18 01:54:19 -0800

    HOLY SHIT SUCCESSFULLY ADDED HOME TAB TO IOS [iOS WORKING]

--- app/components/FooterTabs/FooterTabs.ios.js
+++ app/components/FooterTabs/FooterTabs.ios.js
@@ -1,14 +1,25 @@
 import React, { PropTypes } from 'react';
 import { TabBarIOS, Text } from 'react-native';
+import { colors } from '~/styles';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 FooterTabs.propTypes = {
-
+  activeFooterTab: PropTypes.string.isRequired,
+  navigator: PropTypes.object.isRequired,
+  onTabSelect: PropTypes.func.isRequired
 }
 
 export default function FooterTabs (props) {
   return (
-    <Text>
-      FooterTabs
-    </Text>
+    <TabBarIOS tintColor={colors.active}>
+      <Icon.TabBarItem
+        iconSize={35}
+        iconName='ios-home-outline'
+        title='Home'
+        selected={props.activeFooterTab === 'home'}
+        onPress={() => props.onTabSelect('home')}>
+          <Text>HOME!</Text>
+      </Icon.TabBarItem>
+    </TabBarIOS>
   )
 }
